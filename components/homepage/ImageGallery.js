6abadfab03b552f356d9374690675774241a8931
Chaffexd <shanechaffe@gmail.com>
2023-10-11 16:23:46 +0100

    performance edits on images

--- components/homepage/ImageGallery.js
+++ components/homepage/ImageGallery.js
@@ -5,52 +5,57 @@ const ImageGallery = () => {
     <div className="flex justify-center gap-10 py-4 overflow-hidden">
       <div className="relative aspect-[9/10] flex-none w-1/5 overflow-hidden rounded-xl rotate-2"> 
         <Image
-          src={"/images/me.jpeg"}
+          src={"/images/me.webp"}
           className="h-full w-full object-cover"
           alt="A photo of me, Shane"
           width={400}
           height={400}
           priority
+          sizes="(min-width: 640px) 18rem, 11rem"
         />
       </div>
       <div className="relative aspect-[9/10] flex-none w-1/5 overflow-hidden rounded-xl -rotate-2"> 
         <Image
-          src={"/images/pg.jpeg"}
+          src={"/images/pg.webp"}
           className="h-full w-full object-cover"
           alt="A photo of my Poodle"
           width={400}
           height={400}
+          sizes="(min-width: 640px) 18rem, 11rem"
           priority
         />
       </div>
       <div className="relative aspect-[9/10] flex-none w-1/5 overflow-hidden rounded-xl rotate-2"> 
         <Image
-          src={"/images/berlin.jpg"}
+          src={"/images/berlin.webp"}
           className="h-full w-full object-cover"
           alt="A photo of Berlin"
           width={400}
           height={400}
           priority
+          sizes="(min-width: 640px) 18rem, 11rem"
         />
       </div>
       <div className="relative aspect-[9/10] flex-none w-1/5 overflow-hidden rounded-xl rotate-2"> 
         <Image
-          src={"/images/mountain.jpg"}
+          src={"/images/mountain.webp"}
           className="h-full w-full object-cover"
           alt="A photo of some mountains"
           width={400}
           height={400}
           priority
+          sizes="(min-width: 640px) 18rem, 11rem"
         />
       </div>
       <div className="relative aspect-[9/10] flex-none w-1/5 overflow-hidden rounded-xl -rotate-2"> 
         <Image
-          src={"/images/arsenal.jpg"}
+          src={"/images/arsenal.webp"}
           className="h-full w-full object-cover"
           alt="A photo of my favourite team stadium"
           width={400}
           height={400}
           priority
+          sizes="(min-width: 640px) 18rem, 11rem"
         />
       </div>
     </div>
