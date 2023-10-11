14f732c6914bcc69283b19394b50d2d6aaabcb64
Chaffexd <shanechaffe@gmail.com>
2023-10-11 21:36:49 +0100

    SEO edit

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ export default function Home({ posts }) {
       <Head>
         <title>Shane Chaffe - Software Developer</title>
         <meta name="description" content={"Shane Chaffe's Portfolio - Software Engineer"} />
+        <meta name="keywords" content="React, Next.js, Contentful, Tailwind CSS, Web development" />
+        <meta name="author" content="Shane Chaffe" />
       </Head>
       <Hero posts={posts} />
     </>
