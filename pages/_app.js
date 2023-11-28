001003cf9ea40970827f9c8efc6d19d60aa2b20d
Chaffexd <shanechaffe@gmail.com>
2023-11-28 11:11:23 +0000

    added page loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <NextNProgress 
+        color="#14b8a6"
+        startPosition={0.2}
+        stopDelayMs={100}
+        showOnShallow={false}
+      />
       <Component {...pageProps} />
       <Analytics />
     </Layout>
